fba3eec6803f94cf06ff83eee3bc9ce9abac5607
Nandik <nandik@devbhuti.com>
2021-02-20 09:56:34 +0530

    Updates

---
+++ precache-manifest.2581722732117e8302d3d5389af6cb26.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff24ac6cb21abd942fef0d516d56985b",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "d7eeebd5aa28852d52dd",
+    "url": "/keeper-app/static/js/2.5c858340.chunk.js"
+  },
+  {
+    "revision": "80b6de434740db9e6d0e",
+    "url": "/keeper-app/static/js/main.52204431.chunk.js"
+  },
+  {
+    "revision": "78797e502974e5d55e3c",
+    "url": "/keeper-app/static/js/runtime-main.e57cdbd2.js"
+  }
+]);
